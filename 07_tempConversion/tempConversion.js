d7ce0698052747dac4e205d4ae1f3ab608ef858c
Oshamaru <callMeSam808@gmail.com>
2022-03-22 11:29:08 -1000

    Finish question 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let fTemp = (temp -32) * (5/9);
+  fTemp = Math.round(fTemp * 10) / 10;
+  return fTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let cTemp = (temp * (9/5)) + 32;
+  cTemp = Math.round(cTemp * 10) / 10;
+  return cTemp;
 };
 
 // Do not edit below this line
