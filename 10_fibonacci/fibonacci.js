5c53abbe86e870e5143ae934fe9f2de8a8fcdd9a
Oshamaru <callMeSam808@gmail.com>
2022-03-28 12:37:40 -1000

    Finish question 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let fib = [1, 1];
+    if (num < 0) {
+        return 'OOPS';
+    }
+    if (num == 1) {
+        return fib[0];
+    }
+    if (num == 2) {
+        return fib[1];
+    }
+    for (let i = 1; i < num - 1; i++) {
+        let result = fib[i-1] + fib[i];
+        fib.push(result);
+    }
+    return fib[num - 1];
 };
 
 // Do not edit below this line
