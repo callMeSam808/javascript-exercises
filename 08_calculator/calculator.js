7a8df354296b6875521eead588e04d27b28626c6
Oshamaru <callMeSam808@gmail.com>
2022-03-27 18:40:59 -1000

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numList) {
+	let listSum = 0;
+  for (let num of numList) {
+    listSum += num;
+  }
+  return listSum;
 };
 
-const multiply = function() {
-
+const multiply = function(numList) {
+  let listProduct = numList[0];
+  for (let i = 1; i < numList.length; i++) {
+    listProduct *= numList[i];
+    }
+  return listProduct;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	let result = base;
+  for (let i = 1; i < power; i++) {
+    result *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+
+const factorial = function(num) {
+	if (num < 2) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = 2; i <= num; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
