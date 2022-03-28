4f361707a3d373c3c25362fe7e74b37c45b87e51
Oshamaru <callMeSam808@gmail.com>
2022-03-28 11:50:24 -1000

    Create test html doc and finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let noPuncStr = string.replace(/\W/g, "");
+    let splitString = noPuncStr.split('');
+    let reverseString = '';
+    for (let i = splitString.length - 1; i >= 0; i--) {
+        reverseString += splitString[i];
+    }
+    return reverseString.toUpperCase() === noPuncStr.toUpperCase();
 };
 
 // Do not edit below this line
