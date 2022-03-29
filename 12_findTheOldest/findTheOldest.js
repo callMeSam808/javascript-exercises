0bc1a222b0284a3247f4cf40aa615552ee4bd9bf
Oshamaru <callMeSam808@gmail.com>
2022-03-28 15:24:28 -1000

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestPerson = people[0];
+    
+    people.forEach(person => {
+        if (!person.yearOfDeath) {
+            let currentDate = new Date();
+            person.yearOfDeath = currentDate.getFullYear();
+        }
+        let currentAge = person.yearOfDeath - person.yearOfBirth;
+        let oldestAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+        if (oldestAge < currentAge) 
+            oldestPerson = person;
+        });
+    return oldestPerson;
 };
 
 // Do not edit below this line
